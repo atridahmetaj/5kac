02e8b978e28d6a919efca0d7729f1b4db1f906b2
atridahmetaj <atrid1995@gmail.com>
2019-01-01 23:33:04 +0100

    Strukture ide mbi activity kryesore

--- app/src/main/java/com/example/crs/a5kac/MainActivity.java
+++ app/src/main/java/com/example/crs/a5kac/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.crs.a5kac;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,6 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //first push
+        //main activity mendoj te kete 2 butona ku do zgjedhi nese do luaje solo apo multiplayer
+        //solo e con ne activitet tjeter ku do zgjedhi veshtiresine
+        // multiplayer e con ne activityn DHOMA ku do ftoje shoket ose random match(mbetet per tu diskutuar)
+
     }
 }

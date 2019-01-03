189837e0bc11afd6f90e331510f5de95ef4618ef
atridahmetaj <atrid1995@gmail.com>
2019-01-03 23:00:06 +0100

    Strukture ide mbi activity kryesore

--- app/src/main/java/com/example/crs/a5kac/MainActivity.java
+++ app/src/main/java/com/example/crs/a5kac/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.crs.a5kac;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         //main activity mendoj te kete 2 butona ku do zgjedhi nese do luaje solo apo multiplayer
         //solo e con ne activitet tjeter ku do zgjedhi veshtiresine
